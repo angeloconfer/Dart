ee9eef7c5b80af083d27dbc49e68c5e3294b9fde
angeloconfer <angeloconfer@hotmail.com>
2020-04-18 18:13:40 -0300

    Dart

--- orientacao_objetos/bin/classes_data.dart
+++ orientacao_objetos/bin/classes_data.dart
@@ -9,5 +9,17 @@ class Data {
 main(){
   var dataAniversario = Data();
   dataAniversario.dia = 11;
-  data
+  dataAniversario.mes = 01;
+  dataAniversario.ano = 1983;
+
+  Data dataCompra = Data();
+  dataCompra.dia = 23;
+  dataCompra.mes = 03;
+  dataCompra.ano = 2021;
+
+
+  print('${dataAniversario.dia}/${dataAniversario.mes}/${dataAniversario.ano}');
+  print('${dataCompra.dia}/${dataCompra.mes}/${dataCompra.ano}');
+
+
 }
